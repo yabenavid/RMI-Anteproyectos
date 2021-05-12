43f645842578cd63911a61eb00d3cc0dc6e6917f
Yeviles <yaviles@unicauca.edu.co>
2021-05-12 10:26:15 -0500

    modificaciones en el SGestionAnteproyectos

--- RMI-Anteproyectos/src/cliente/Cliente.java
+++ RMI-Anteproyectos/src/cliente/Cliente.java
@@ -5,10 +5,37 @@
  */
 package cliente;
 
+import SGestionAnteproyectos.sop_rmi.GestionAnteproyectosInt;
+import SSeguimientoAnteproyectos.dto.FormatoTIA;
+import cliente.presentacion.jfrIniciarSesion;
+import cliente.presentacion.jfrmVistaInicial;
+
 /**
  *
- * @author ANDRES BENAVIDEZ
+ * @author ANDRES BENAVIDEZ, YENNYFER
  */
 public class Cliente {
+    	private static GestionAnteproyectosInt objRemotoAnteproyecto;
+        private static FormatoTIA objAnteproyecto;
     
+	public static void main(String args[]){
+		//if (System.getSecurityManager()==null)
+		//{
+		//	System.setSecurityManager(new RMISecurityManager());
+		//}
+
+        int numPuertoRMIRegistry = 0;
+        String direccionIpRMIRegistry = "";
+
+            jfrmVistaInicial vistaInicicial =  new jfrmVistaInicial();
+            vistaInicicial.setVisible(true);
+        
+//        System.out.println("Cual es la direccion ip donde se encuentra el rmiregistry ");
+//        direccionIpRMIRegistry = UtilidadesConsola.leerCadena();
+//        System.out.println("Cual es el numero del puerto por el cual escucha el rmiregistry ");
+//        numPuertoRMIRegistry = UtilidadesConsola.leerEntero();
+//        
+//        objRemotoAnteproyecto = (GestionAnteproyectosInt) UtilidadesRegistroC.obtenerObjRemoto(direccionIpRMIRegistry, numPuertoRMIRegistry, "ObjetoRemotoAnteproyecto");
+//        //menuPrincipal();
+}
 }
---
+++ RMI-Anteproyectos/src/cliente/utilidades/Validaciones.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente.utilidades;
+
+/**
+ *
+ * @author YENNYFER
+ */
+public class Validaciones {
+    public static boolean isNumeric(String cadena){
+	try {
+		Integer.parseInt(cadena);
+		return true;
+	} catch (NumberFormatException nfe){
+		return false;
+	}
+}
+}
